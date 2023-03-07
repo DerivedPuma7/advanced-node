e43e043abceab2becd09e05ae234c528324d9a4c
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-07 12:25:48 -0300

    feat: add facebook authentication feature

---
+++ src/domain/features/facebook-authetication.ts
@@ -0,0 +1,14 @@
+import { AccessToken } from "@/domain/models";
+import { AuthenticationError } from "@/domain/errors";
+
+export interface FacebookAuthentication {
+   perform: (params: FacebookAuthentication.Params) => Promise<FacebookAuthentication.Result>
+}
+
+namespace FacebookAuthentication {
+   export type Params = {
+      token: string
+   }
+
+   export type Result = AccessToken | AuthenticationError;
+}
