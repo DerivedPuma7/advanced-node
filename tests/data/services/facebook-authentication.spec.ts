6da5582c030ca106dca81168a59ef7f00ea1c621
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-07 15:54:48 -0300

    refactor: adjust exports and imports

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,5 +1,6 @@
 import { AuthenticationError } from "@/domain/errors";
 import { FacebookAuthentication } from "@/domain/features";
+import { LoadFacebookUserApi } from "@/data/contracts/apis";
 
 class FacebookAuthenticationService {
    constructor(
@@ -11,18 +12,6 @@ class FacebookAuthenticationService {
       await this.loadFacebookUserApi.loadUser(params);
       return new AuthenticationError();
    }
-}
-
-interface LoadFacebookUserApi {
-   loadUser: (params: LoadFacebookUserApi.Params) => Promise<LoadFacebookUserApi.Result>;
-}
-
-namespace LoadFacebookUserApi {
-   export type Params = {
-      token: string
-   }
-
-   export type Result = undefined;
 }
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
