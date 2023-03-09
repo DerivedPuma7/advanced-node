244d7dc2d9c1a4ba193b3f7d66b7e1499e536469
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-09 15:03:46 -0300

    ensure AccessToken is created correctely

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,6 @@
-export type AccessToken = {
-   accessToken: string
+export class AccessToken {
+   static expirationInMin: number = 30;
+   static expirationInMs: number = this.expirationInMin * 60 * 1000;
+
+   constructor(private readonly value: string) {}
 }
---
+++ tests/domain/models/access-token.spec.ts
@@ -0,0 +1,17 @@
+import { AccessToken } from "@/domain/models";
+
+describe('AccessToken', () => {
+   it('should create with a value', () => {
+      const sut = new AccessToken('any_value');
+
+      expect(sut).toEqual({ value: 'any_value' });
+   });
+
+   it('should expire in 30 minutes', () => {
+      const minutesToExpire = 30;
+      const minutesToMilisseconds = 60 * 1000;
+      const milisseconds = minutesToExpire * minutesToMilisseconds;
+
+      expect(AccessToken.expirationInMs).toBe(milisseconds);
+   });
+});
