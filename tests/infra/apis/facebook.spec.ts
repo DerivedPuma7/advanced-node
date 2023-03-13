4b80631e6b043fa4b3d44c6be505dc136889b08d
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-13 12:42:06 -0300

    feat: ensure FacebookApi get appToken with correct url

---
+++ tests/infra/apis/facebook.spec.ts
@@ -0,0 +1,38 @@
+import { LoadFacebookUserApi } from "@/data/contracts/apis";
+
+import { mock } from "jest-mock-extended";
+
+class FacebookApi {
+   private readonly baseUrl = 'https://graph.facebook.com';
+
+   constructor(
+      private readonly httpClient: HttpGetClient
+   ) {}
+
+   async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
+      await this.httpClient.get({ url: `${this.baseUrl}/oauth/access_token` })
+   }
+}
+
+interface HttpGetClient {
+   get: (params: HttpGetClient.params) => Promise<void>
+}
+
+namespace HttpGetClient {
+   export type params = {
+      url: string
+   }
+}
+
+describe('FacebookApi', () => {
+   it('should get app token', async () => {
+      const httpClient = mock<HttpGetClient>();
+      const sut = new FacebookApi(httpClient);
+
+      await sut.loadUser({ token: 'any_client_token' });
+
+      expect(httpClient.get).toHaveBeenCalledWith({
+         url: 'https://graph.facebook.com/oauth/access_token',
+      });
+   });
+});
