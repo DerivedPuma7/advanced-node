9884e699698ff0f1e969cd6beee00938a9237333
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-15 14:58:43 -0300

    fix: fix atribute visibility

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -2,5 +2,5 @@ export class AccessToken {
    static expirationInMin: number = 30;
    static expirationInMs: number = this.expirationInMin * 60 * 1000;
 
-   constructor(private readonly value: string) {}
+   constructor(readonly value: string) {}
 }
