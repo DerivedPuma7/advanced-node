3d7a5f0ed227e52abde04101d99dfe88d113f48a
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-15 16:57:44 -0300

    feat: ensure FacebookLoginController returns 500 on serverError

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -6,27 +6,42 @@ import { mock, MockProxy } from "jest-mock-extended";
 class FacebookLoginController {
    constructor(private readonly facebookAuthentication: FacebookAuthentication){}
    async handle(httpRequest: any): Promise<HttpResponse> {
-      if(httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
-         return {
-            statusCode: 400,
-            data: new Error('The field token is required')
-         };
-      }
+      try {
+         if(httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
+            return {
+               statusCode: 400,
+               data: new Error('The field token is required')
+            };
+         }
 
-      const result = await this.facebookAuthentication.perform({ token: httpRequest.token });
-      if(result instanceof AccessToken) {
-         return {
-            statusCode: 200,
-            data: {
-               accessToken: result.value
+         const result = await this.facebookAuthentication.perform({ token: httpRequest.token });
+         if(result instanceof AccessToken) {
+            return {
+               statusCode: 200,
+               data: {
+                  accessToken: result.value
+               }
             }
          }
-      }
 
-      return {
-         statusCode: 401,
-         data: result
-      };
+         return {
+            statusCode: 401,
+            data: result
+         };
+      } catch (error: any) {
+         return {
+            statusCode: 500,
+            data: new ServerError(error)
+         };
+      }
+   }
+}
+
+class ServerError extends Error {
+   constructor(error?: Error) {
+      super('Server failed, try again soon');
+      this.name = 'ServerError';
+      this.stack = error?.stack;
    }
 }
 
@@ -94,6 +109,17 @@ describe('FacebookLoginController', () => {
          data: {
             accessToken: 'any_value'
          }
+      });
+   });
+
+   it('should return 500 if authetication throws', async () => {
+      const error = new Error('infra_error');
+      facebookAuthentication.perform.mockRejectedValueOnce(error);
+      const httpResponse = await sut.handle({ token: 'any_token' });
+
+      expect(httpResponse).toEqual({
+         statusCode: 500,
+         data: new ServerError(error)
       });
    });
 
