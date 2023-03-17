613752e371db999d027a06d649597d8b521b23c4
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-17 15:27:50 -0300

    feat: create facebook login integration tests

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+   facebookApi: {
+      clientId: process.env.FB_CLIENT_ID ?? '692835359190626',
+      clientSecret: process.env.FB_CLIENT_SECRET ?? '6cbfa3a103abea3dfc5eb8ac95e66e91'
+   }
+};
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,44 @@
+import { FacebookApi } from "@/infra/apis";
+import { AxiosHttpClient } from "@/infra/http";
+import { env } from "@/main/config/env";
+
 describe('', () => {
-   it('teste', () => {
-      
+   let axiosClient: AxiosHttpClient;
+   let sut: FacebookApi;
+
+   beforeEach(() => {
+      axiosClient = new AxiosHttpClient();
+      sut = new FacebookApi(
+         axiosClient,
+         env.facebookApi.clientId,
+         env.facebookApi.clientSecret
+      );
+   });
+   
+   /**
+    * Teste não desenvolvido
+    * Motivo: serviço do facebook developers não me permitiu criar usuários de teste
+    */
+   // it('should return a facebook user if token is valid', async () => {
+   //    const axiosClient = new AxiosHttpClient();
+   //    const sut = new FacebookApi(
+   //       axiosClient,
+   //       env.facebookApi.clientId,
+   //       env.facebookApi.clientSecret
+   //    );
+
+   //    const fbUser = await sut.loadUser({ token: '' });
+
+   //    expect(fbUser).toEqual({
+   //       facebookId: '',
+   //       name: '',
+   //       email: ''
+   //    });
+   // });
+
+   it('should return undefined if token is invalid', async () => {
+      const fbUser = await sut.loadUser({ token: 'invalid_token' });
+
+      expect(fbUser).toBeUndefined();
    });
 });
