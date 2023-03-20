10488cb1a5ebae16b9fbbb1c6e1c1d0a7b833c83
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-20 19:49:09 -0300

    fix: fixing forgotten test

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -15,26 +15,22 @@ describe('', () => {
       );
    });
    
-   /**
-    * Teste não desenvolvido
-    * Motivo: serviço do facebook developers não me permitiu criar usuários de teste
-    */
-   // it('should return a facebook user if token is valid', async () => {
-   //    const axiosClient = new AxiosHttpClient();
-   //    const sut = new FacebookApi(
-   //       axiosClient,
-   //       env.facebookApi.clientId,
-   //       env.facebookApi.clientSecret
-   //    );
+   it('should return a facebook user if token is valid', async () => {
+      const axiosClient = new AxiosHttpClient();
+      const sut = new FacebookApi(
+         axiosClient,
+         env.facebookApi.clientId,
+         env.facebookApi.clientSecret
+      );
 
-   //    const fbUser = await sut.loadUser({ token: '' });
+      const fbUser = await sut.loadUser({ token: 'EAAJ2IU147mIBAJez7mrTMpXCtXsOgokZAZBszZCfuPZCgkJ8bURLo6XP870tEaDUvoKUxtifzDZClXvj6VKvgBsX8K129gTP87P39XMwIC4s2jM5KSJkYOya38SgrQcyUjuEuGtguHi0HW1frHz9wDZAuxFtapKJtYko2ugZBCB5YLfEyZCtcdffYZBfl6fpH2G0rAHiZAUOygPi0iyu07EZC8x6aEVvfXZCbPRHePlU6mYw4pNpM8J4Srh6yF1QGKegDF49ryF05vzcsAZDZD' });
 
-   //    expect(fbUser).toEqual({
-   //       facebookId: '',
-   //       name: '',
-   //       email: ''
-   //    });
-   // });
+      expect(fbUser).toEqual({
+         facebookId: '3290661811226038',
+         name: 'Gustavo Figueiredo',
+         email: 'gustavoribeiro665@hotmail.com'
+      });
+   });
 
    it('should return undefined if token is invalid', async () => {
       const fbUser = await sut.loadUser({ token: 'invalid_token' });
