f15eb01f528b4d2a6003c794370f13941085fd6f
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-20 11:32:53 -0300

    feat: add env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,5 +3,6 @@ export const env = {
       clientId: process.env.FB_CLIENT_ID ?? '692835359190626',
       clientSecret: process.env.FB_CLIENT_SECRET ?? '6cbfa3a103abea3dfc5eb8ac95e66e91'
    },
-   port: process.env.PORT ?? 8080
+   port: process.env.PORT ?? 8080,
+   jwtSecret: process.env.JWT_SECRET ?? 'qualquerchavesupersecreta'
 };
