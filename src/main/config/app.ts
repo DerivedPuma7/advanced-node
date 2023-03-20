afc50b45ff5756c3875f31c13032f6655fc5ae95
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-20 10:44:36 -0300

    feat: configure express server

---
+++ src/main/config/app.ts
@@ -0,0 +1,10 @@
+import { setupMiddlewares } from "@/main/config/middlewares";
+import { setupRoutes } from "@/main/config/routes";
+
+import express from "express";
+
+const app = express();
+setupMiddlewares(app);
+setupRoutes(app);
+
+export { app };
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,9 @@
+import "./config/module-alias";
+import { app } from "@/main/config/app";
+import { env } from "@/main/config/env";
+
 import "reflect-metadata";
+
+app.listen(env.port, () => {
+   console.log(`Server running at http://localhost/${env.port}`);
+});
