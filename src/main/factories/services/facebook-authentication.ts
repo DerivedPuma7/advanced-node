7f1d990c0a422c01c850a7286272b7ff6c56fc49
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-20 11:31:58 -0300

    feat: create FacebookAuthenticationService factory

---
+++ src/main/factories/services/facebook-authentication.ts
@@ -0,0 +1,16 @@
+import { FacebookAuthenticationService } from "@/data/services";
+import { makeFacebookApi } from "@/main/factories/apis";
+import { makePgUserAccountRepository } from "@/main/factories/repos";
+import { makeJwtTokenGenerator } from "@/main/factories/crypto";
+
+export const makeFacebookAuthenticationService = (): FacebookAuthenticationService => {
+   const facebookApi = makeFacebookApi();
+   const pgUserAccountRepository = makePgUserAccountRepository();
+   const jwtTokenGenerator = makeJwtTokenGenerator();
+
+   return new FacebookAuthenticationService(
+      facebookApi,
+      pgUserAccountRepository,
+      jwtTokenGenerator
+   );
+};
