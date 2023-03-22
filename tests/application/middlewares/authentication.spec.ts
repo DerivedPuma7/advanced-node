f600c755aabfb7d88b67c7a4ec571928298885ea
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-22 11:35:39 -0300

    feat: ensure AuthenticationMiddleware returns 403 if authorize throws

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -10,7 +10,11 @@ class AuthenticationMiddleware {
    async handle({ authorization }: HttpRequest): Promise<HttpResponse<Error> | undefined> {
       const error = new RequiredStringValidator(authorization, 'authorization').validate();
       if(error) return forbidden();
-      await this.authorize({ token : authorization});
+      try {
+         await this.authorize({ token : authorization});
+      } catch {
+         return forbidden();
+      }
    }
 }
 
@@ -60,5 +64,16 @@ describe('AuthenticationMiddleware', () => {
 
       expect(authorize).toHaveBeenCalledWith({ token: authorization});
       expect(authorize).toHaveBeenCalledTimes(1);
+   });
+
+   it('should return 403 if authorize throws', async () => {
+      authorize.mockRejectedValueOnce(new Error('any_error'));
+
+      const httpResponse = await sut.handle({ authorization });
+
+      expect(httpResponse).toEqual({
+         statusCode: 403,
+         data: new ForbiddenError()
+      });
    });
 });
