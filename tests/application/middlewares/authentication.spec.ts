f829dcdcba0f900b4b224c2a87424448ba521bbc
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-22 11:46:10 -0300

    refactor: improve code quality

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -8,15 +8,20 @@ type Model = Error | { userId: string };
 
 class AuthenticationMiddleware {
    constructor(private readonly authorize: Authorize) {}
+
    async handle({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
-      const error = new RequiredStringValidator(authorization, 'authorization').validate();
-      if(error) return forbidden();
+      if(! this.validate({ authorization })) return forbidden();
       try {
          const userId = await this.authorize({ token : authorization});
          return ok({ userId });
       } catch {
          return forbidden();
       }
+   }
+
+   private validate({ authorization }: HttpRequest): boolean {
+      const error = new RequiredStringValidator(authorization, 'authorization').validate();
+      return error === undefined;
    }
 }
 
