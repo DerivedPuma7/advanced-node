ab12448635bb74ae0c454c36705729ef64c5af99
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-23 12:05:04 -0300

    feat: make middleware implement correct interface

--- src/application/middlewares/authentication.ts
+++ src/application/middlewares/authentication.ts
@@ -1,11 +1,12 @@
 import { forbidden, HttpResponse, ok } from "@/application/helpers";
 import { RequiredStringValidator } from "@/application/validation";
 import { Authorize } from "@/domain/use-cases";
+import { Middleware } from "@/application/middlewares";
 
 type HttpRequest = { authorization: string };
 type Model = Error | { userId: string };
 
-export class AuthenticationMiddleware {
+export class AuthenticationMiddleware implements Middleware {
    constructor(private readonly authorize: Authorize) {}
 
    async handle({ authorization }: HttpRequest): Promise<HttpResponse<Model>> {
