2a1be5d8d6697c98f8c4a81f1de6c552808b8a59
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-23 10:26:44 -0300

    refactor: isolate sut creation

--- tests/main/adapters/express-middleware.spec.ts
+++ tests/main/adapters/express-middleware.spec.ts
@@ -1,8 +1,8 @@
 import { HttpResponse } from "@/application/helpers";
 
 import { getMockReq, getMockRes } from "@jest-mock/express";
-import { RequestHandler } from "express";
-import { mock } from "jest-mock-extended";
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import { mock, MockProxy } from "jest-mock-extended";
 
 type Adapter = (middleware: Middleware) => RequestHandler;
 const adaptExpressMiddleware: Adapter = middleware => {
@@ -16,13 +16,25 @@ interface Middleware {
 }
 
 describe('ExpressMiddleware', () => {
-   it('should call handle with correct request', async () => {
-      const req = getMockReq({ headers: { any: 'any' } });
-      const res = getMockRes().res;
-      const next = getMockRes().next;
-      const middleware = mock<Middleware>();
-      const sut = adaptExpressMiddleware(middleware);
+   let req: Request;
+   let res: Response;
+   let next: NextFunction;
+   let middleware: MockProxy<Middleware>;
+   let sut: RequestHandler;
 
+   beforeAll(() => {
+      req = getMockReq({ headers: { any: 'any' } });
+      res = getMockRes().res;
+      next = getMockRes().next;
+      middleware = mock<Middleware>();
+      sut = adaptExpressMiddleware(middleware);
+   });
+
+   beforeEach(() => {
+      sut = adaptExpressMiddleware(middleware);
+   });
+
+   it('should call handle with correct request', async () => {
       await sut(req, res, next);
 
       expect(middleware.handle).toHaveBeenCalledWith({ any: 'any' });
@@ -30,12 +42,7 @@ describe('ExpressMiddleware', () => {
    });
 
    it('should call handle with empty request', async () => {
-      const req = getMockReq({});
-      const res = getMockRes().res;
-      const next = getMockRes().next;
-      const middleware = mock<Middleware>();
-      const sut = adaptExpressMiddleware(middleware);
-
+      req = getMockReq({});
       await sut(req, res, next);
 
       expect(middleware.handle).toHaveBeenCalledWith({});
