6a070f30e66818ca88c184025f2202fe187e07b6
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-27 11:32:40 -0300

    feat: ensure ChangeProfilePicture calls LoadUserProfile with correct input

--- src/domain/contracts/repos/user-profile.ts
+++ src/domain/contracts/repos/user-profile.ts
@@ -5,3 +5,11 @@ export interface SaveUserPicture {
 export namespace SaveUserPicture {
    export type Input = { pictureUrl?: string }
 }
+
+export interface LoadUserProfile {
+   load: (params: LoadUserProfile.Input) => Promise<void>;
+}
+
+export namespace LoadUserProfile {
+   export type Input = { id: string }
+}
--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -1,7 +1,7 @@
 import { UploadFile, UUIDGenerator } from "@/domain/contracts/gateways";
-import { SaveUserPicture } from "@/domain/contracts/repos";
+import { SaveUserPicture, LoadUserProfile } from "@/domain/contracts/repos";
 
-type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture) => ChangeProfilePicture;
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator, userProfileRepo: SaveUserPicture & LoadUserProfile) => ChangeProfilePicture;
 export type ChangeProfilePicture = (input: Input) => Promise<void>;
 type Input = { userId: string, file?: Buffer };
 
@@ -12,5 +12,6 @@ export const setupChangeProfilePicture: Setup = (fileStorage, crypto, userProfil
          pictureUrl = await fileStorage.upload({ file, key: crypto.uuid({ key: userId }) });
       }
       await userProfileRepo.savePicture({ pictureUrl });
+      await userProfileRepo.load({ id: userId });
    }
 }
