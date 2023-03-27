57b04011e9b3251e70a3e94d241d3b75bc6800bc
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-27 11:24:04 -0300

    feat: ensure ChangeProfilePicture calls SavePicture with correct initials

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -42,7 +42,7 @@ describe('ChangeProfilePicture', () => {
    it('should call SavePicture with correct input', async () => {
       await sut({ userId: 'any_id', file });
 
-      expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: 'any_url' });
+      expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: 'any_url', initials: undefined });
       expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1);
    });
 
