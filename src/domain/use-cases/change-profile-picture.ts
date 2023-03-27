dc02e22ba78ac70ceaced47fee5c333adba62796
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-27 10:55:35 -0300

    feat: ensure ChangeProfilePicture dont call UploadFile if file is undefined

--- src/domain/use-cases/change-profile-picture.ts
+++ src/domain/use-cases/change-profile-picture.ts
@@ -2,10 +2,12 @@ import { UploadFile, UUIDGenerator } from "@/domain/contracts/gateways";
 
 type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture;
 export type ChangeProfilePicture = (input: Input) => Promise<void>;
-type Input = { userId: string, file: Buffer };
+type Input = { userId: string, file?: Buffer };
 
 export const setupChangeProfilePicture: Setup = (fileStorage, crypto): ChangeProfilePicture => {
    return async ({ userId, file }): Promise<void> => {
-      await fileStorage.upload({ file, key: crypto.uuid({ key: userId }) });
+      if(file) {
+         await fileStorage.upload({ file, key: crypto.uuid({ key: userId }) });
+      }
    }
 }
--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -28,4 +28,10 @@ describe('ChangeProfilePicture', () => {
       expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid });
       expect(fileStorage.upload).toHaveBeenCalledTimes(1);
    });
+
+   it('should not call UploadFile if file is undefined', async () => {
+      await sut({ userId: 'any_id', file: undefined });
+
+      expect(fileStorage.upload).not.toHaveBeenCalled();
+   });
 });
