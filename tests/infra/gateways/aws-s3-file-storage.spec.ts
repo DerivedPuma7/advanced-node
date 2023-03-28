00eb482037b125424d2097c053619c5257eae357
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-28 14:43:18 -0300

    refactor: isolate sut creation

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -17,12 +17,20 @@ class AwsS3FileStorage {
 }
 
 describe('AwsS3FileStorage', () => {
+   let sut: AwsS3FileStorage;
+   let accessKey: string;
+   let secret: string;
+
+   beforeAll(() => {
+      accessKey = 'any_access_key';
+      secret = 'any_secret';
+   });
+
+   beforeEach(() => {
+      sut = new AwsS3FileStorage(accessKey, secret);
+   });
+
    it('should config aws credentials on creation', async () => {
-      const accessKey = 'any_access_key';
-      const secret = 'any_secret';
-
-      const sut = new AwsS3FileStorage(accessKey, secret);
-
       expect(sut).toBeDefined();
       expect(config.update).toHaveBeenCalledWith({
          credentials: {
