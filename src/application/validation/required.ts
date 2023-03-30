55aa7a41d1dc0a98170438952b10d0036aa88278
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-30 10:30:55 -0300

    feat: add RequiredBuffer validator

--- src/application/validation/required.ts
+++ src/application/validation/required.ts
@@ -30,3 +30,19 @@ export class RequiredString extends Required {
       return undefined;
    }
 }
+
+export class RequiredBuffer extends Required {
+   constructor(
+      override readonly value: Buffer,
+      override readonly fieldName?: string
+   ) {
+      super(value, fieldName);
+   }
+
+   override validate(): Error | undefined {
+      if (super.validate() !== undefined || this.value.length === 0) {
+         return new RequiredFieldError(this.fieldName);
+      }
+      return undefined;
+   }
+}
--- tests/application/validation/required.spec.ts
+++ tests/application/validation/required.spec.ts
@@ -1,5 +1,5 @@
 import { RequiredFieldError } from "@/application/errors";
-import { Required, RequiredString } from "@/application/validation";
+import { Required, RequiredString, RequiredBuffer } from "@/application/validation";
 
 describe('Required', () => {
    it('should return RequiredFieldError if value is null', async () => {
@@ -44,6 +44,30 @@ describe('RequiredString', () => {
 
    it('should return undefined if value is not empty', async () => {
       const sut = new RequiredString('any_value', 'any_field');
+
+      const error = sut.validate();
+
+      expect(error).toBeUndefined();
+   });
+});
+
+describe('RequiredBuffer', () => {
+   it('should extend Required', async () => {
+      const sut = new RequiredBuffer(Buffer.from(''));
+
+      expect(sut).toBeInstanceOf(Required);
+   });
+
+   it('should return RequiredFieldError if value is empty', async () => {
+      const sut = new RequiredBuffer(Buffer.from(''));
+
+      const error = sut.validate();
+
+      expect(error).toEqual(new RequiredFieldError());
+   });
+
+   it('should return undefined if value is not empty', async () => {
+      const sut = new RequiredBuffer(Buffer.from('any_buffer'));
 
       const error = sut.validate();
 
