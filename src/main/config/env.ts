912ba2a96029ee8943cf334d24bcbebfdc9c87d9
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-03-31 13:00:05 -0300

    feat: add aws s3 credentials

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,15 @@
+import "dotenv/config";
+
 export const env = {
    facebookApi: {
-      clientId: process.env.FB_CLIENT_ID ?? '692835359190626',
-      clientSecret: process.env.FB_CLIENT_SECRET ?? '6cbfa3a103abea3dfc5eb8ac95e66e91'
+      clientId: process.env.FB_CLIENT_ID ?? '',
+      clientSecret: process.env.FB_CLIENT_SECRET ?? ''
    },
    port: process.env.PORT ?? 8080,
-   jwtSecret: process.env.JWT_SECRET ?? 'qualquerchavesupersecreta'
+   jwtSecret: process.env.JWT_SECRET ?? '',
+   s3: {
+      accessKey: process.env.AWS_S3_ACCESS_KEY ?? '',
+      secret: process.env.AWS_S3_SECRET ?? '',
+      bucket: process.env.AWS_S3_BUCKET ?? ''
+   }
 };
