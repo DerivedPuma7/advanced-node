73254a07e10001f8e8e85d06747b57e02b5266f6
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-04-03 15:45:44 -0300

    feat: ensure PUT /users/picture returns 200 on success

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,5 +1,5 @@
 import { makeSavePictureController } from "@/main/factories/application/controllers";
-import { adaptExpressRoute } from "@/main/adapters";
+import { adaptExpressRoute, adaptMulter as upload } from "@/main/adapters";
 import { auth } from "@/main/middlewares";
 
 import { Router } from "express";
@@ -7,5 +7,5 @@ import { Router } from "express";
 export default (router: Router): void  => {
    const controller = makeSavePictureController();
    router.delete('/users/picture', auth, adaptExpressRoute(controller));
-   router.put('/users/picture', auth, adaptExpressRoute(controller));
+   router.put('/users/picture', auth, upload, adaptExpressRoute(controller));
 }
--- tests/main/routes/user.spec.ts
+++ tests/main/routes/user.spec.ts
@@ -48,11 +48,33 @@ describe('User Routes', () => {
    });
 
    describe('[PUT] /users/picture', () => {
+      const uploadSpy = jest.fn();
+
+      jest.mock('@/infra/gateways/aws-s3-file-storage', () => ({
+         AwsS3FileStorage: jest.fn().mockReturnValue({
+            upload: uploadSpy
+         })
+      }));
+
       it('should return 403 if no Authorization header is present', async () => {
          const { status } = await request(app)
             .put('/api/users/picture');
 
          expect(status).toBe(403);
+      });
+
+      it('should return 200 with valid data', async () => {
+         uploadSpy.mockResolvedValueOnce('any_aws_s3_url');
+         const { id } = await pgUserRepo.save({ email: 'any_email', name: 'gustavo Figueiredo' });
+         const authorization = sign({ key: id }, env.jwtSecret);
+
+         const { status, body } = await request(app)
+            .put('/api/users/picture')
+            .set({ authorization})
+            .attach('picture', Buffer.from('any_buffer'), { filename: 'any_name', contentType: 'image/png' });
+
+         expect(status).toBe(200);
+         expect(body).toEqual({ pictureUrl: 'any_aws_s3_url', initials: undefined });
       });
    });
 });
