2a10b42fe96cc4e664f3534b2474bb3396801506
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-04-04 11:49:39 -0300

    refactor: move code to production files

---
+++ src/infra/repos/postgres/helpers/connection.ts
@@ -0,0 +1,31 @@
+import { ConnectionNotFoundError } from "@/infra/repos/postgres/helpers"
+
+import { Connection, QueryRunner, createConnection, getConnection, getConnectionManager } from "typeorm";
+
+export class PgConnection {
+   private static instance?: PgConnection;
+   private query?: QueryRunner;
+
+   private constructor() {}
+
+   static getInstance(): PgConnection {
+      if(PgConnection.instance === undefined) {
+         PgConnection.instance = new PgConnection();
+      }
+      return PgConnection.instance;
+   }
+
+   async connect(): Promise<void> {
+      const connection: Connection = getConnectionManager().has('default')
+         ? getConnection()
+         : await createConnection();
+
+      this.query = connection.createQueryRunner();
+   }
+
+   async disconnect(): Promise<void> {
+      if(this.query === undefined) throw new ConnectionNotFoundError();
+      await getConnection().close();
+      this.query = undefined;
+   }
+}
--- tests/infra/repos/postgres/helpers/connection.spec.ts
+++ tests/infra/repos/postgres/helpers/connection.spec.ts
@@ -1,4 +1,5 @@
-import { Connection, QueryRunner, createConnection, getConnection, getConnectionManager } from "typeorm";
+import { createConnection, getConnection, getConnectionManager } from "typeorm";
+import { PgConnection, ConnectionNotFoundError } from "@/infra/repos/postgres/helpers"
 
 jest.mock('typeorm', () => ({
    Entity: jest.fn(),
@@ -8,41 +9,6 @@ jest.mock('typeorm', () => ({
    getConnection: jest.fn(),
    getConnectionManager: jest.fn(),
 }));
-
-class ConnectionNotFoundError extends Error {
-   constructor() {
-      super('No connection was found');
-      this.name = 'ConnectionNotFoundError';
-   }
-}
-
-class PgConnection {
-   private static instance?: PgConnection;
-   private query?: QueryRunner;
-
-   private constructor() {}
-
-   static getInstance(): PgConnection {
-      if(PgConnection.instance === undefined) {
-         PgConnection.instance = new PgConnection();
-      }
-      return PgConnection.instance;
-   }
-
-   async connect(): Promise<void> {
-      const connection: Connection = getConnectionManager().has('default')
-         ? getConnection()
-         : await createConnection();
-
-      this.query = connection.createQueryRunner();
-   }
-
-   async disconnect(): Promise<void> {
-      if(this.query === undefined) throw new ConnectionNotFoundError();
-      await getConnection().close();
-      this.query = undefined;
-   }
-}
 
 describe('PgConnection', () => {
    let getConnectionManagerSpy: jest.Mock;
